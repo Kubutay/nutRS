4c298843ecb49fee4f9d6987e4deff3f8a8cd567
kubutay <enola.akk@gmail.com>
2020-03-03 15:55:08 +0100

    Pushing the full module

---
+++ substitutabilityScore/XP_substitution_generation.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Sema
+
+Generate substitutability scores from a .p file
+
+1. Preprocess data 
+2. Compute substitutabilityScore 
+    score =  [jaccardIndex, jaccardIndex2]
+3. Output in the form of a dict
+
+"""
+import os
+import pickle
+import datetime
+import logging
+
+
+
+date = datetime.datetime.now().strftime('%y_%m_%d_%H_%M')
+filename = 'substitutabilityScore/logs/XP_' + date + '.log'
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+logging.getLogger().setLevel(logging.DEBUG)
+logging.basicConfig(filename=filename, 
+                    format = '%(asctime)s %(message)s', 
+                    level=logging.INFO)
+
+from substitutabilityScore import jaccardIndex, openPickleFile, saveToDict, main
+
+filepath_dico = 'data/dict_cod.p'
+filepath_data = 'data/conso_ad.p'
+level = 'codsougr'
+max_meal = 10
+score = jaccardIndex
+
+### Import data
+dict_cod = openPickleFile(filepath_dico)
+conso = openPickleFile(filepath_data)
+dico = dict_cod[level]
+
+res = main(conso, level, dico, max_meal, score)
+res_dict = saveToDict(res)
+
+with open('results/subScoreAllMeals1.p', 'wb') as handle:
+    pickle.dump(res_dict, handle)
